8eb6f76854bd1d59122c7380933746b8f612a1cf
llister15 <llister@wonkasoft.com>
2018-03-31 05:55:01 -0700

    init

---
+++ includes/class-gift-recorder-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wonkasoft.com
+ * @since      1.0.0
+ *
+ * @package    Gift_Recorder
+ * @subpackage Gift_Recorder/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Gift_Recorder
+ * @subpackage Gift_Recorder/includes
+ * @author     Wonkasoft <support@wonkasoft.com>
+ */
+class Gift_Recorder_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-gift-recorder-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://wonkasoft.com
+ * @since      1.0.0
+ *
+ * @package    Gift_Recorder
+ * @subpackage Gift_Recorder/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Gift_Recorder
+ * @subpackage Gift_Recorder/includes
+ * @author     Wonkasoft <support@wonkasoft.com>
+ */
+class Gift_Recorder_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'gift-recorder',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
