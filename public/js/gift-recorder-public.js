63332f8a61149033b9076ca591a8a32a8bd3de4e
MrLister <m_r_lister@yahoo.com>
2018-10-22 03:44:58 -0700

    MrLister 10-22-2018 3:44am

--- public/js/gift-recorder-public.js
+++ public/js/gift-recorder-public.js
@@ -12,18 +12,57 @@
 			header_img_link = header_img_link.replace( /gatewaysHeader/gi, 'gatewaysHeader2' );
 			header_img.src = header_img_link;
 
-			process_effects(get_content, 'fade', 350 );
-			;
+			process_instructions(get_content, 'in', 350 );
+			
+			get_content.onclick = function (e) {
+				e.preventDefault;
+				if ( get_content.classList.contains( 'in' ) ) {
+					process_instructions(get_content, 'out', 350 );
+					return;
+				}
+				if ( get_content.classList.contains( 'out' ) ) {
+					process_instructions(get_content, 'in', 350 );
+					return;
+				}
+			};
 		};
+
 	}
 
 	function getting_started() {
 
 	}
 
-	function process_effects( element, effect, time ) {
-		console.log(element);
-		element.style.overflow = 'hidden';
-		element.style.height = '50px';
+	function process_instructions( element, effect, time ) {
+		console.log(effect);
+		if ( effect == 'in' ) {
+			element.style.overflow = 'hidden';
+			element.style.height = '50px';
+			element.style.width = '150px';
+			element.style.position = 'fixed';
+			element.style.top = '50%';
+			element.style.right = 0;
+			element.style.transform = 'rotate(-90deg)';
+			element.classList.add( 'in' );
+			if ( element.classList.contains( 'out') ) {
+				element.classList.remove( 'out' );
+			}
+			return;
+		}
+		if ( effect == 'out' ) {
+			element.style.overflow = 'hidden';
+			element.style.height = '100%';
+			element.style.width = '500px';
+			element.style.position = 'fixed';
+			element.style.top = '50%';
+			element.style.right = '50%';
+			element.style.transform = 'rotate(0deg)';
+			element.classList.add( 'out' );
+			if ( element.classList.contains( 'in') ) {
+				element.classList.remove( 'in' );
+			}
+			return;
+		}
+
 	}
 })( jQuery );
